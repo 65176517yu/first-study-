958549e02a0f92c1fa0fb93ecd54f6fded886ba6
masuda <yuuu_mom@yahoo.co.jp>
2014-05-24 16:06:21 +0900

    hello

--- hello.c
+++ hello.c
@@ -2,8 +2,14 @@
 
 int main()
 {
-	for(int i=0;i<10;i++){
-		printf("hello world\n");
+	int i,j;
+
+	printf("Š|ŽZ‚Ì‹ã‹ã‚Ì•\Ž¦\n");
+
+	for (i = 1; i < 10;i++){
+		for (j = 1; j < 10;j++){
+			printf("%d~%d=%d\n",i,j,i*j);
+		}
 	}
 	return 0;
 }
